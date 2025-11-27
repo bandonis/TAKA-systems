05c530f268ca12b088d48548de38fb2db6f6bc50
bandonis <ivars.bandonis@gmail.com>
2025-11-27 14:30:03 +0200

    sync changes

--- app/(tenant)/events/[eventId]/edit/page.tsx
+++ app/(tenant)/events/[eventId]/edit/page.tsx
@@ -31,12 +31,9 @@ async function getEvent(eventId: string) {
   return event;
 }
 
-type EditPageProps = {
-  params: { eventId: string };
-};
-
-export default async function EditEventPage({ params }: EditPageProps) {
-  const event = await getEvent(params.eventId);
+export default async function Page({ params }: { params: { eventId: string } }) {
+  const { eventId } = params;
+  const event = await getEvent(eventId);
 
   return (
     <section className="space-y-6">
